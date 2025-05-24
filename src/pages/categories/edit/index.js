2328928e4095ca6340b3986a2b7faec58b6ab869
P-Nastia <nastia.pyvovarchuck@gmail.com>
2025-05-24 12:23:19 +0300

    is good modiefied edit

--- src/pages/categories/edit/index.js
+++ src/pages/categories/edit/index.js
@@ -4,161 +4,141 @@ import axiosInstance from "../../../api/axiosInstance";
 import BaseTextInput from "../../../components/common/baseTextInput";
 import BaseFileInput from "../../../components/common/baseFileInput";
 import {BASE_URL} from "../../../api/apiConfig";
-
+import {useFormik} from "formik";
 
 const CategoriesEdit = () => {
     const { id } = useParams();
+
     const [currentImage, setCurrentImage] = useState("");
-    const [formData, setFormData] = useState({
+    const initValues = {
         id: 0,
-        name: '',
-        image: null,
-        slug: ''
-    });
+        name: "",
+        slug: "",
+        imageFile: null
+    }
+
     useEffect(() => {
         axiosInstance.get(`/api/categories/${id}`)
             .then(res => {
                 const data = res.data;
-                setFormData({
+                setValues({
                     id: data.id,
                     name: data.name,
                     slug: data.slug,
-                    image: null
+                    imageFile: null
                 });
                 setCurrentImage(data.image);
                 console.log('Get category:', data.image);
             })
             .catch(err => console.error('Error loading category:', err));
     }, [id]);
-    const [errors, setErrors] = useState({
-        name: '',
-        slug: '',
-        image:''
+
+    const handleFormikSubmit = async (values) => {
+
+        console.log("Formik submit", values);
+        try {
+            var result = await axiosInstance.put(`${BASE_URL}/api/categories/edit`, values,
+                {
+                    headers: {
+                        "Content-Type": "multipart/form-data"
+                    }
+                });
+            console.log("Server result", result);
+            navigate("..");
+        }
+        catch (err) {
+            console.log("Error", err);
+            const serverErrors = {};
+            const {response} = err;
+            const {data} = response;
+            if(data) {
+                const {errors} = data;
+                Object.entries(errors).forEach(([key, messages]) => {
+                    let messageLines = "";
+                    messages.forEach(message => {
+                        messageLines += message+" ";
+                        console.log(`${key}: ${message}`);
+                    });
+                    const field = key.charAt(0).toLowerCase() + key.slice(1);
+                    serverErrors[field] = messageLines;
+
+                });
+            }
+            setErrors(serverErrors);
+        }
+    }
+    const formik=useFormik({
+        initialValues: initValues,
+        onSubmit: handleFormikSubmit,
+        enableReinitialize : true,
+
     });
+
+    const {values,handleSubmit,errors,setErrors,touched,handleChange,setFieldValue,setValues} = formik; //values - зміни, які будуть в форміку
+
 
     const navigate = useNavigate();
 
-    const handleOnChange=(e)=>{
-        setFormData({
-            ...formData,
-            [e.target.name]:e.target.value
-        })
-    }
-
     const handleFileChange=(e)=>{
         const files=e.target.files;
+        console.log("File change",files[0]);
+
         if(files.length>0){
-            setFormData({
-                ...formData,
-                [e.target.name]:files[0]
-            })
-            setCurrentImage("");
+            setFieldValue("imageFile",files[0]);
         }
         else{
-            console.log(files[0]);
-            setFormData({
-                ...formData,
-                [e.target.name]:null
-            })
+            setFieldValue("imageFile",null);
         }
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const data = new FormData();
-        data.append("id", formData.id);
-        data.append("name", formData.name);
-        data.append("slug", formData.slug);
-        if (formData.image) {
-            data.append("imageFile", formData.image);
-        }
-
-        try {
-            await axiosInstance.post("/api/categories/edit", data, {
-                headers: {
-                    "Content-Type": "multipart/form-data"
-                }
-            });
-
-            navigate("..");
-        } catch (err) {
-            const newErrors = {};
-            const errors=err.response.data.errors;
-            if (errors && typeof errors === 'object') {
-
-                if (errors.Name) {
-                    newErrors.name = errors.Name[0];
-                    console.log(errors.Name[0]);
-                }
-
-                if (errors.Slug) {
-                    newErrors.slug = errors.Slug[0];
-                    console.log(errors.Slug[0]);
-                }
-
-                if (errors.ImageFile) {
-                    newErrors.image = errors.ImageFile[0];
-                    console.log(errors.ImageFile[0]);
-                }
-
-            }
-            else{
-                console.log(err.response.data);
-                newErrors.name = err.response.data;
-            }
-            setErrors(newErrors);
-        }
-    };
-
     return (
-        <div >
+        <form className={"col-md-6 offset-md-3 mt-4"} onSubmit={handleSubmit}>
+            <h2 className={"text-center"}>Create category</h2>
+            <BaseTextInput
+                field={"name"}
+                label={"Name"}
+                handleOnChange={handleChange}
+                value={values.name}
+                error={errors.name}
+                touched={touched.name}
+            />
 
-            <form className={"col-md-6 offset-md-3 mt-4"} onSubmit={handleSubmit}>
-                <h2 className={"text-center"}>Edit category</h2>
-                <BaseTextInput
-                    field={"name"}
-                    label={"Name"}
-                    handleOnChange={handleOnChange}
-                    value={formData.name}
-                    error={errors.name}
+            <BaseTextInput
+                field={"slug"}
+                label={"Slug"}
+                handleOnChange={handleChange}
+                value={values.slug}
+                error={errors.slug}
+                touched={touched.slug}
+            />
+
+            <BaseFileInput
+                field={"image"}
+                label={"Choose image"}
+                handleOnChange={handleFileChange}
+                error={errors.imageFile}
+                touched={touched.imageFile}
+            />
+
+            <div className="mb-3">
+                <img
+                    src={
+                        values.imageFile
+                            ? URL.createObjectURL(values.imageFile) : `${BASE_URL}/images/200_${currentImage}`
+                    }
+                    alt=""
+                    width={100}
                 />
+            </div>
 
-                <BaseTextInput
-                    field={"slug"}
-                    label={"Slug"}
-                    handleOnChange={handleOnChange}
-                    value={formData.slug}
-                    error={errors.slug}
-                />
-
-                <BaseFileInput
-                    field={"image"}
-                    label={"Choose image"}
-                    handleOnChange={handleFileChange}
-                    error={errors.image}
-                />
-                <div className="mb-3">
-                    <img
-                        src={
-                            formData.image
-                                ? URL.createObjectURL(formData.image)
-                                : `${BASE_URL}/images/200_${currentImage}`
-                        }
-                        alt=""
-                        width={100}
-                    />
-                </div>
-
-
-                <button type="submit" className="btn btn-primary">
-                    Edit
-                </button>
-                <Link to="/categories" className="btn btn-primary ms-2">
-                    Cancel
-                </Link>
-            </form>
-        </div>
+            <button type="submit" className="btn btn-primary">
+                                    Edit
+            </button>
+            <Link to="/categories" className="btn btn-primary ms-2">
+                                 Cancel
+            </Link>
+        </form>
     );
-}
+};
+
 export default CategoriesEdit;
